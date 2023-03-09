9930a21d91e86a0a1479c515b3d6169c0d504dcf
Snehal979 <bansodsnehal535@gmail.com>
2023-03-10 03:36:32 +0530

    [Snehal]Add:State Population

---
+++ IndianStatesProgram/CSVStateCensus.cs
@@ -0,0 +1,32 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStatesProgram.Files_StatePopulationn
+{
+    public class CSVStateCensus
+    {
+        /// <summary>
+        /// Uc1 State Cencus Data Population
+        /// </summary>
+        /// <param name="filepath"></param>
+        /// <returns></returns>
+        public void ReadStateCencusData(string filepath)
+        {
+            using (var reader = new StreamReader(filepath))
+            using (var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csvReader.GetRecords<StatePopulationData>().ToList();
+                foreach (var data in records)
+                {
+                    Console.WriteLine("State: "+data.State+"\nPopulation: "+data.Population+"\nArea: "+data.AreaInSqKm+"\nDensity:"+data.DensityPerSqKm+"\n ");
+                }
+            }
+        }
+    }
+}
--- IndianStatesProgram/Program.cs
+++ IndianStatesProgram/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace IndianStateProgram
+﻿using IndianStatesProgram.Files_StatePopulationn;
+
+namespace IndianStateProgram
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Indian State Program");
+            string filePopulationPath = @"C:\Users\hp\Desktop\newBatch2\IndianStatesProgram\IndianStatesProgram\Files_StatePopulationn\StatePopulationFile.csv";
+            CSVStateCensus cSVStateCensus = new CSVStateCensus();
+            cSVStateCensus.ReadStateCencusData(filePopulationPath);
+            Console.ReadLine();
         }
     }
 }
